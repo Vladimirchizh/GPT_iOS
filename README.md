46cbded2a35c5ac784eaca57bb7e7a1d6a24d3ca
Vladimir <vdchizhevskiy@gmail.com>
2021-08-15 15:29:56 +0300

    adding template converter

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Installing venv, with transformers, torch and gdown(package to download files from google drive )
+
     source run_first.sh
+
+Running script
+
+    python conversion.py
--- run_first.sh
+++ run_first.sh
@@ -1,3 +1,5 @@
 python3 -m venv gpt_ios
 source gpt_ios/bin/activate
 pip3 install -r requirements.txt
+# cd models/infobuzz && gdown https://drive.google.com/uc\?id\=1P8tnBY-N3bca4Q_o2B7wIYi0HIdnHjCC
+# downloading model from google drive
